0d3ffc1a6b86eb1dfa9987a3257b7161cad206ef
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-23 02:43:26 +0600

    intrigate left side navbar

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [catagories, setCatagories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/catagories')
+            .then(res => res.json())
+            .then(data => setCatagories(data))
+            .catch(error => console.error(error))
+    }, [])
+
     return (
         <div>
-            
+            <h3>All Catagory</h3>
+          <div className='ps-4'>
+          {
+                catagories.map(catagory => <p
+                 key={catagory.id}
+                >
+                    <Link to={`/catagory/${catagory.id}`} className='text-black text-decoration-none'>{catagory.name}</Link>
+                </p>)
+            }
+          </div>
         </div>
     );
 };
