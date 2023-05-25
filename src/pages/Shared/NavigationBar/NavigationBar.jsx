9ccea7ef5ea76e6593e3f03805fbf83afe5d97ef
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-25 15:20:53 +0600

    added navigation bar using login form

---
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+
+const NavigationBar = () => {
+    const {user} = useContext(AuthContext);
+    return (
+        <Container>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                                <Link to="/">Home</Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {user && <FaUserCircle
+                                style={{ fontSize: '2rem' }}
+                            ></FaUserCircle>}
+                                {user ?
+                                    <Button varient="secondary">Logout</Button> :
+                                    <Link to="/login">
+                                        <Button varient="secondary">Login</Button>
+                                    </Link>
+                                }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
+    );
+};
+
+export default NavigationBar;
